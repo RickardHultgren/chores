4d896f28e07d85383e574862b9f034b033fd229a
Rickard Hultgren <sababa.sababa@gmail.com>
2021-02-01 10:17:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -40,21 +40,21 @@ def Insert_data():
 	c.execute('CREATE TABLE IF NOT EXISTS RecordONE (item_id text,item_name text,item_category text,item_begin text,item_expire text,item_quantity text,item_Unit text,item_place text)')
 	c.execute("INSERT INTO RecordONE (item_id,item_name,item_category,item_begin,item_expire,item_quantity,item_Unit,item_place) VALUES(?, ?, ?, ?, ?, ?, ?, ? )", (ITEM_ID_VALUE , ITEM_NAME_VALUE , ITEM_CATEGORY , ITEM_BEGIN , ITEM_EXPIRE , ITEM_QUANTITY , ITEM_UNIT , ITEM_PLACE))
 	#c.execute("INSERT INTO stock VALUES ( ITEM_ID_VALUE , ITEM_NAME_VALUE , ITEM_CATEGORY , ITEM_BEGIN , ITEM_EXPIRE , ITEM_QUANTITY , ITEM_UNIT , ITEM_PLACE )")
-	c.commit()
+	conn.commit()
 
 def Update_data():
 	conn  = sqlite3.connect('test.db')
 	c = conn.cursor()
 	#global c
 	c.execute('update stock set ITEM_Id = ? ,ITEM_Name = ?,ITEM_Begin = ?,ITEM_Expire = ?  roduct_Quantity = ?,ITEM_Unit = ?,ITEM_Place where ITEM_Id = ?',(ITEM_ID_VALUE.get(),ITEM_NAME_VALUE.get(),ITEM_CATEGORY.get(),ITEM_BEGIN.get(),ITEM_EXPIRE.get(),ITEM_QUANTITY.get(),ITEM_UNIT.get(),ITEM_PLACE.get()))
-	c.commit()
+	conn.commit()
 
 def Delete_data():
 	conn  = sqlite3.connect('test.db')
 	c = conn.cursor()
 	#global c
 	c.execute('delete from stock where ITEM_Id = ?',( ITEM_ID_VALUE.get(),))
-	c.commit()
+	conn.commit()
 
 if __name__=='__main__':
 	#Get_data()
@@ -103,6 +103,8 @@ if __name__=='__main__':
 			Update_data()
 		if sys.argv[x] == "-l":
 			Get_data()
+	
+
 	
 
 	
