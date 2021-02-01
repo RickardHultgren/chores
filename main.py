a7ab7992df429dc78e30e4e65e6ee1b4653d1ed6
Rickard Hultgren <sababa.sababa@gmail.com>
2021-02-01 06:04:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,33 +5,6 @@ import sys
 
 #******************************** Get DATA *******************************
 i=0
-def Get_data():
-	global i
-	global j
-	i=0
-	db = sqlite3.connect('test.db')
-	cursor = db.execute('select * from stock',[1,2,3,4,5,6,7])
-	for row in cursor:
-		text="Item_"+str(i)
-		values=(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7])
-		print(text+values)
-		i=i+1
-
-def Insert_data():
-	db = sqlite3.connect('test.db')
-	db.execute('insert into stock (ITEM_Id,ITEM_Name,ITEM_Category,ITEM_Begin,ITEM_Expire,ITEM_Quantity,ITEM_Unit,ITEM_Place) values (?,?,?,?,?,?,?,?,?)',[ITEM_ID_VALUE.get(),ITEM_NAME_VALUE.get(),ITEM_CATEGORY.get(),ITEM_BEGIN.get(),ITEM_EXPIRE.get(),ITEM_QUANTITY.get(),ITEM_UNIT.get(),ITEM_PLACE.get()])
-	db.commit()
-
-def Update_data():
-	db = sqlite3.connect('test.db')
-	db.execute('update stock set ITEM_Id = ? ,ITEM_Name = ?,ITEM_Begin = ?,ITEM_Expire = ?  roduct_Quantity = ?,ITEM_Unit = ?,ITEM_Place where ITEM_Id = ?',(ITEM_ID_VALUE.get(),ITEM_NAME_VALUE.get(),ITEM_CATEGORY.get(),ITEM_BEGIN.get(),ITEM_EXPIRE.get(),ITEM_QUANTITY.get(),ITEM_UNIT.get(),ITEM_PLACE.get()))
-	db.commit()
-
-def Delete_data():
-	db = sqlite3.connect('test.db')
-	db.execute('delete from stock where ITEM_Id = ?',( ITEM_ID_VALUE.get(),))
-	db.commit()
-
 ITEM_ID_VALUE = str()
 ITEM_NAME_VALUE = str()
 ITEM_CATEGORY = str()
@@ -42,8 +15,52 @@ ITEM_UNIT = str()
 ITEM_PLACE = str()
 argv_len = len(sys.argv)
 
+conn  = sqlite3.connect('test.db')
+c = conn.cursor()
+
+def Get_data():
+	global i
+	global j
+	i=0
+	#conn  = sqlite3.connect('test.db')
+	#c = conn.cursor()
+	global c
+	#cursor = db.execute('select * from stock',[1,2,3,4,5,6,7])
+	for row in c.execute('SELECT * FROM stocks ORDER BY item_name'):
+		print(row)
+		#i=i+1
+
+def Insert_data():
+	#conn  = sqlite3.connect('test.db')
+	#c = conn.cursor()
+	global c
+	c.execute("INSERT INTO stock VALUES ( ITEM_ID_VALUE , ITEM_NAME_VALUE , ITEM_CATEGORY , ITEM_BEGIN , ITEM_EXPIRE , ITEM_QUANTITY , ITEM_UNIT , ITEM_PLACE )")
+	c.commit()
+
+def Update_data():
+	#conn  = sqlite3.connect('test.db')
+	#c = conn.cursor()
+	global c
+	c.execute('update stock set ITEM_Id = ? ,ITEM_Name = ?,ITEM_Begin = ?,ITEM_Expire = ?  roduct_Quantity = ?,ITEM_Unit = ?,ITEM_Place where ITEM_Id = ?',(ITEM_ID_VALUE.get(),ITEM_NAME_VALUE.get(),ITEM_CATEGORY.get(),ITEM_BEGIN.get(),ITEM_EXPIRE.get(),ITEM_QUANTITY.get(),ITEM_UNIT.get(),ITEM_PLACE.get()))
+	c.commit()
+
+def Delete_data():
+	#conn  = sqlite3.connect('test.db')
+	#c = conn.cursor()
+	global c
+	c.execute('delete from stock where ITEM_Id = ?',( ITEM_ID_VALUE.get(),))
+	c.commit()
+
 if __name__=='__main__':
 	#Get_data()
+	global c
+	try:
+		c.execute('''CREATE TABLE stocks
+			(item_id text,item_name text,item_category text,item_begin text,item_expire text,item_quantity text,item_Unit text,item_place)''')
+	except:
+		pass
+	conn.commit
+	conn.close
 	dblength = i
 	for x in range(0, argv_len):
 		if sys.argv[x] == "-f":
