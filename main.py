ca61a5473f94c3ecc217d5b217196823d19f7498
Rickard Hultgren <sababa.sababa@gmail.com>
2021-02-05 05:47:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -85,6 +85,7 @@ def Insert_data():
 	except:
 		pass
 	c.execute('CREATE TABLE IF NOT EXISTS Things (item_id text,item_name text,item_category text,item_begin text,item_expire text,item_quantity text,item_unit text,item_place text)')
+	conn.commit()
 	c.execute("INSERT INTO Things (item_id,item_name,item_category,item_begin,item_expire,item_quantity,item_unit,item_place) VALUES(?, ?, ?, ?, ?, ?, ?, ? )", (ITEM_ID , ITEM_NAME_VALUE , ITEM_CATEGORY , ITEM_BEGIN , ITEM_EXPIRE , ITEM_QUANTITY , ITEM_UNIT , ITEM_PLACE))
 	#c.execute("INSERT INTO stock VALUES ( ITEM_ID , ITEM_NAME_VALUE , ITEM_CATEGORY , ITEM_BEGIN , ITEM_EXPIRE , ITEM_QUANTITY , ITEM_UNIT , ITEM_PLACE )")
 	conn.commit()
@@ -160,33 +161,34 @@ def Insert_recipe():
 	c.execute('CREATE TABLE IF NOT EXISTS Ings (ri_ing text, the_ing text)')
 	c.execute('CREATE TABLE IF NOT EXISTS Amounts (ri_amount text, the_amount text)')
 	c.execute('CREATE TABLE IF NOT EXISTS Units (ri_unit text, the_unit text)')
+	conn.commit()
 
-  c.execute("INSERT INTO Recipes (rec_id,rec_name,rec_category,rec_ingredients,rec_instruction,rec_time,rec_unit,rec_place) VALUES(?, ?, ?, ?, ?, ?, ? )", (REC_ID , REC_NAME_VALUE , REC_CATEGORY , REC_INS , REC_TIME , REC_UNIT , REC_PLACE))
-
-  for indexing, ing_row in REC_ING["ingredients"]:
-    RI_ING = REC_ING["ingredients"][indexing]
-    RI_AMOUNT = REC_ING["amounts"][indexing]
-    RI_UNIT = REC_ING["units"][indexing]
-  	c.execute("INSERT INTO Rec_ing () VALUES(?, ?, ?, ? )", (REC_ID, RI_ING, RI_AMOUNT, RI_UNIT)
-
-  for indexing, ing_row in REC_ING["ingredients"]:
-    RI_ING = REC_ING["ingredients"][indexing]
-	  c.execute("INSERT INTO Units () VALUES(?, ? )", (RI_ING, THE_ING)
-
-  for indexing, ing_row in REC_ING["amounts"]:
-    RI_AMOUNT = REC_ING["amounts"][indexing]
-	  c.execute("INSERT INTO Amounts () VALUES(?, ? )", (RI_AMOUNT, THE_AMOUNT)
-
-  for indexing, ing_row in REC_ING["unts"]:
-    RI_UNIT = REC_ING["units"][indexing]
-	  c.execute("INSERT INTO Units () VALUES(?, ? )", (RI_UNIT, THE_UNIT)
-
-
-
-
-
+	c.execute("INSERT INTO Recipes (rec_id,rec_name,rec_category,rec_instruction,rec_time,rec_unit,rec_place) VALUES(?, ?, ?, ?, ?, ?, ? )", (REC_ID , REC_NAME_VALUE , REC_CATEGORY , REC_INS , REC_TIME , REC_UNIT , REC_PLACE))
+	
+	for indexing, ing_row in enumerate(REC_ING["ingredients"]):
+		RI_ING = REC_ING["ingredients"][indexing]
+		RI_AMOUNT = REC_ING["amounts"][indexing]
+		RI_UNIT = REC_ING["units"][indexing]
+		c.execute("INSERT INTO Rec_ing (rec_id, ri_ing, ri_amount, ri_unit) VALUES(?, ?, ?, ? )", (REC_ID, RI_ING, RI_AMOUNT, RI_UNIT))
+	
+	for indexing, ing_row in enumerate(REC_ING["ingredients"]):
+		RI_ING = REC_ING["ingredients"][indexing]
+		c.execute("INSERT INTO Ings (ri_ing, the_ing) VALUES(?, ? )", (RI_ING, THE_ING))
+	
+	for indexing, ing_row in enumerate(REC_ING["amounts"]):
+		RI_AMOUNT = REC_ING["amounts"][indexing]
+		c.execute("INSERT INTO Amounts (ri_amount, the_amount) VALUES(?, ? )", (RI_AMOUNT, THE_AMOUNT))
+	
+	for indexing, ing_row in enumerate(REC_ING["units"]):
+		RI_UNIT = REC_ING["units"][indexing]
+		c.execute("INSERT INTO Units (ri_unit, the_unit) VALUES(?, ? )", (RI_UNIT, THE_UNIT))
+	
+	
+	
+	
+	
 	#####
-  #c.execute("INSERT INTO stock VALUES ( ITEM_ID , ITEM_NAME_VALUE , ITEM_CATEGORY , ITEM_BEGIN , ITEM_EXPIRE , ITEM_QUANTITY , ITEM_UNIT , ITEM_PLACE )")
+	#c.execute("INSERT INTO stock VALUES ( ITEM_ID , ITEM_NAME_VALUE , ITEM_CATEGORY , ITEM_BEGIN , ITEM_EXPIRE , ITEM_QUANTITY , ITEM_UNIT , ITEM_PLACE )")
 	conn.commit()
 
 def Update_recipe():
@@ -197,9 +199,70 @@ def Update_recipe():
 	conn.commit()
 
 def Delete_recipe():
+
 	conn  = sqlite3.connect('test.db')
 	c = conn.cursor()
 	#global c
+	
+	c.execute('delete from Ings where ri_ing = ?',( RI_ING))
+	try:
+		c.execute('create table temp_Ings as select ri_ing, the_ing from Ings order by ri_ing')
+	except:
+		pass
+	c.execute('drop table Ings')
+	c.execute('CREATE TABLE IF NOT EXISTS Ings (ri_ing text, the_ing text)')
+	c.execute("INSERT INTO Ings (ri_ing, the_ing) VALUES(?, ? )", (RI_ING, THE_ING))
+
+
+	c.execute('delete from Units where ri_ing = ?',( RI_AMOUNT))
+	try:
+		c.execute('create table temp_Units as select ri_amount, the_amount from Ings order by ri_amount')
+	except:
+		pass
+	c.execute('drop table Amount')
+	c.execute('CREATE TABLE IF NOT EXISTS Units (ri_amount text, the_amount text)')
+	c.execute("INSERT INTO Amount (ri_amount, the_amount) VALUES(?, ? )", (RI_AMOUNT, THE_AMOUNT))
+
+
+	c.execute('delete from Units where ri_unit = ?',( RI_UNIT))
+	try:
+		c.execute('create table temp_Units as select ri_ing, the_ing from Ings order by ri_ing')
+	except:
+		pass
+	c.execute('drop table Units')
+	c.execute('CREATE TABLE IF NOT EXISTS Units (ri_unit text, the_unit text)')
+	c.execute("INSERT INTO Ings (ri_unit, the_unit) VALUES(?, ? )", (RI_UNIT, THE_UNIT))
+
+###
+	c.execute('CREATE TABLE IF NOT EXISTS Rec_Ing (rec_id text, ri_ing text, ri_amount text, ri_unit)')
+	c.execute('delete from Rec_Ings where rec_id = ?',( REC_ID))
+	try:
+		c.execute('create table temp_Rec_Ings as select rec_id text, ri_ing text, ri_amount text, ri_unit from Ings order by rec_id')
+	except:
+		pass
+	c.execute('drop table Rec_Ings')
+	c.execute('CREATE TABLE IF NOT EXISTS Rec_Ings (rec_id text, ri_ing text, ri_amount text, ri_unit)')
+	c.execute("INSERT INTO Rec_Ings (rec_id, ri_ing, ri_amount, ri_unit) VALUES(?, ?, ?, ?)", (REC_ID, RI_ING, RI_AMOUNT, RI_UNIT))
+
+	
+	c.execute('delete from Recipes where rec_id = ?',( REC_ID))
+	try:
+		c.execute('create table temp_Recipes as select rec_id,rec_name,rec_category,rec_instruction,rec_time,rec_unit,rec_place from Recipes order by rec_id')
+	except:
+		pass
+	c.execute('drop table Recipes')
+	c.execute('CREATE TABLE IF NOT EXISTS Recipes (rec_id text,rec_name text,rec_category text,rec_instruction text,rec_time text,rec_unit text,rec_place text)')
+	c.execute("INSERT INTO Recipes (rec_id,rec_name,rec_category,rec_instruction,rec_time,rec_unit,rec_place) select rowid,rec_id,rec_name,rec_category,rec_instruction,rec_time,rec_unit,rec_place from temp_Recipes order by rowid")
+	
+	
+	
+	
+	#####
+	#c.execute("INSERT INTO stock VALUES ( ITEM_ID , ITEM_NAME_VALUE , ITEM_CATEGORY , ITEM_BEGIN , ITEM_EXPIRE , ITEM_QUANTITY , ITEM_UNIT , ITEM_PLACE )")
+	conn.commit()
+
+
+###
 	c.execute('delete from Recipes where rec_id = ?',( ITEM_ID))
 	try:
 		c.execute('create table temp_Recipes as select rec_id,rec_name,rec_category,rec_ingredients,rec_instruction,rec_time,rec_unit,rec_place from Recipes order by rec_id')
@@ -306,70 +369,114 @@ if __name__=='__main__':
 		if sys.argv[x] == "-ra":
 			ITEM_ID = str(dblength+1)
 			try:
-				ITEM_NAME_VALUE = input("Name of the item:")
-				ITEM_CATEGORY = input("Category of the item:")
-				ITEM_BEGIN = input("Start date:")
-				ITEM_EXPIRE = input("Expiring date:")
-				ITEM_QUANTITY = input("Quantity:")
-				ITEM_UNIT = input("Unit:")
-				ITEM_PLACE = input("Place:")
+				count = 0 
+				REC_NAME_VALUE = input("Name of the recipe:")
+				REC_CATEGORY = input("Category of the recipe:")
+				print("Ingredients:")
+	        
+				while 1:
+					a_ing = input("name of ingredient" + str(count))	
+					if a_ing == "" :
+					  break
+					REC_ING["ingredients"].append(a_ing)
+					a_amount = input("amount of ingredient" + str(count))
+					REC_ING["amounts"].append(a_amount)
+					a_unit = input("unit of ingredient" + str(count))
+					REC_ING["units"].append(a_unit)
+					count += 1
+	
+				#REC_ING = input("Ingredients:")
+				REC_INS = input("Instructions")
+				REC_TIME = input("Quantity:")
+				REC_UNIT = input("Unit:")
+				REC_PLACE = input("Place:")
 			except:
-				ITEM_NAME_VALUE = raw_input("Name of the item:")
-				ITEM_CATEGORY = raw_input("Category of the item:")
-				ITEM_BEGIN = raw_input("Start date:")
-				ITEM_EXPIRE = raw_input("Expiring date:")
-				ITEM_QUANTITY = raw_input("Quantity:")
-				ITEM_UNIT = raw_input("Unit:")
-				ITEM_PLACE = raw_input("Place:")
+				REC_NAME_VALUE = raw_input("Name of the recipe:")
+				REC_CATEGORY = raw_input("Category of the recipe:")
+				print("Ingredients:")
+	        
+				while 1:
+					a_ing = raw_input("name of ingredient" + str(count))	
+					if a_ing == "" :
+						break
+					REC_ING["ingredients"].append(a_ing)
+					a_amount = raw_input("amount of ingredient" + str(count))
+					REC_ING["amounts"].append(a_amount)
+					a_unit = raw_input("unit of ingredient" + str(count))
+					REC_ING["units"].append(a_unit)
+					count += 1
+				#REC_ING = raw_input("Start date:")
+				REC_INS = raw_input("Expiring date:")
+				REC_TIME = raw_input("Quantity:")
+				REC_UNIT = raw_input("Unit:")
+				REC_PLACE = raw_input("Place:")
 			Insert_recipe()			
 		if sys.argv[x] == "-rd":
 			REC_ID = sys.argv[x+1]  
 			Delete_data()
-	if sys.argv[x] == "-ru":
-		REC_ID = sys.argv[x+1]
-		try:
-			count2 = 0 
-			REC_NAME_VALUE = input("Name of the recipe:")
-			REC_CATEGORY = input("Category of the recipe:")
-			print("Ingredients:")
-        
-			while 1:
-				a_ing = input("name of ingredient %s", str(count))	
-				if a_ing == "" :
-				  break
-			REC_ING["ingredients"].append(a_ing)
-			a_amount = input("amount of ingredient %s", str(count))
-			REC_ING["amounts"].append(a_amount)
-			a_unit = input("unit of ingredient %s", str(count))
-			REC_ING["units"].append(a_unit)
-			count2 += 1
-
-			#REC_ING = input("Ingredients:")
-			REC_INS = input("Instructions")
-			REC_TIME = input("Quantity:")
-			REC_UNIT = input("Unit:")
-			REC_PLACE = input("Place:")
-		except:
-			REC_NAME_VALUE = raw_input("Name of the recipe:")
-			REC_CATEGORY = raw_input("Category of the recipe:")
-			print("Ingredients:")
-        
-			while 1:
-				a_ing = input("name of ingredient %s", str(count))	
-				if a_ing == "" :
-				  break
-			REC_ING["ingredients"].append(a_ing)
-			a_amount = input("amount of ingredient %s", str(count))
-			REC_ING["amounts"].append(a_amount)
-			a_unit = input("unit of ingredient %s", str(count))
-			REC_ING["units"].append(a_unit)
-			count2 += 1
-			#REC_ING = raw_input("Start date:")
-			REC_INS = raw_input("Expiring date:")
-			REC_TIME = raw_input("Quantity:")
-			REC_UNIT = raw_input("Unit:")
-			REC_PLACE = raw_input("Place:")
-			Update_recipe()
+		if sys.argv[x] == "-ru":
+			ITEM_ID = str(dblength+1)
+			try:
+				count = 0 
+				REC_NAME_VALUE = input("Name of the recipe:")
+				REC_CATEGORY = input("Category of the recipe:")
+				print("Ingredients:")
+	        
+				while 1:
+					a_ing = input("name of ingredient" + str(count))	
+					if a_ing == "" :
+					  break
+					REC_ING["ingredients"].append(a_ing)
+					a_amount = input("amount of ingredient" + str(count))
+					REC_ING["amounts"].append(a_amount)
+					a_unit = input("unit of ingredient" + str(count))
+					REC_ING["units"].append(a_unit)
+					count += 1
+	
+				#REC_ING = input("Ingredients:")
+				REC_INS = input("Instructions")
+				REC_TIME = input("Quantity:")
+				REC_UNIT = input("Unit:")
+				REC_PLACE = input("Place:")
+			except:
+				REC_NAME_VALUE = raw_input("Name of the recipe:")
+				REC_CATEGORY = raw_input("Category of the recipe:")
+				print("Ingredients:")
+	        
+				while 1:
+					a_ing = raw_input("name of ingredient" + str(count))	
+					if a_ing == "" :
+						break
+					REC_ING["ingredients"].append(a_ing)
+					a_amount = raw_input("amount of ingredient" + str(count))
+					REC_ING["amounts"].append(a_amount)
+					a_unit = raw_input("unit of ingredient" + str(count))
+					REC_ING["units"].append(a_unit)
+					count += 1
+				#REC_ING = raw_input("Start date:")
+				REC_INS = raw_input("Expiring date:")
+				REC_TIME = raw_input("Quantity:")
+				REC_UNIT = raw_input("Unit:")
+				REC_PLACE = raw_input("Place:")
+			Insert_recipe()			
 	if sys.argv[x] == "-rl":
-		Get_recipe()
+		subarg = str("id")
+		try:
+			if sys.argv[x+1] == "nam":
+				subarg = str("nam")
+			if sys.argv[x+1] == "cat":
+				subarg = str("cat")
+			if sys.argv[x+1] == "beg":
+				subarg = str("beg")
+			if sys.argv[x+1] == "exp":
+				subarg = str("exp")
+			if sys.argv[x+1] == "qua":
+				subarg = str("qua")
+			if sys.argv[x+1] == "uni":
+				subarg = str("uni")
+			if sys.argv[x+1] == "pla":
+				subarg = str("pla")								
+		except:
+			pass
+		Get_recipe(subarg)
 	
